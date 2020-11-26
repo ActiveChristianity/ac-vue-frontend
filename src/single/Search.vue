39a51328ddbdf0ad2ac47d7630c69e9b0dcbe88d
Gerard <gl@munio.no>
2020-11-26 10:51:25 +0100

    Add no results to search

--- src/single/Search.vue
+++ src/single/Search.vue
@@ -11,24 +11,27 @@
              ref="input"
              :placeholder="loading ? 'Loading…' : 'Search'"
       />
-      <ul class="absolute max-h-screen overflow-y-auto bg-white rounded shadow z-50 border-t-2 border-secondary" v-if="searchResults && searchResults.length" style="width:100%">
-        <li v-for="(result, i) in searchResults"
-            @click="close"
-            class="block"
-            :class="{'bg-gray-200 text-secondary': active === i}"
-            @mouseover="active = i"
-        >
-          <g-link
-              :key="result.id"
-              :to="nodePath(result.node)"
-              class="p-2 w-full flex items-center">
-            <div class="rounded-full bg-gray-100 center w-8 h-8 mr-2">
-              <icon v-if="result.node.type" prefix="fal" :name="$typeIcon(result.node.type, false, !! result.node.track)" fa />
-              <p v-else class="uppercase font-semibold font-serif">{{ result.node.title.charAt(0) }}</p>
-            </div>
-            <span>{{ result.title }}</span>
-          </g-link>
-        </li>
+      <ul class="absolute max-h-screen overflow-y-auto bg-white rounded shadow z-50 border-t-2 border-secondary" style="width:100%" v-if="searchTerm.length > 2">
+        <li v-if="! searchResults || ! searchResults.length" class="block text-gray-600 text-center text-sm p-2">No results found</li>
+        <template v-else>
+          <li v-for="(result, i) in searchResults"
+              @click="close"
+              class="block"
+              :class="{'bg-gray-200 text-secondary': active === i}"
+              @mouseover="active = i"
+          >
+            <g-link
+                :key="result.id"
+                :to="nodePath(result.node)"
+                class="p-2 w-full flex items-center">
+              <div class="rounded-full bg-gray-100 center w-8 h-8 mr-2">
+                <icon v-if="result.node.type" prefix="fal" :name="$typeIcon(result.node.type, false, !! result.node.track)" fa />
+                <p v-else class="uppercase font-semibold font-serif">{{ result.node.title.charAt(0) }}</p>
+              </div>
+              <span>{{ result.title }}</span>
+            </g-link>
+          </li>
+        </template>
       </ul>
     </div>
   </transition>
@@ -108,7 +111,9 @@ export default {
     await this.$flexsearch.loadIndex(this.$search)
     this.loading = false
     this.$nextTick(() => {
-      this.$refs.input.focus()
+      if (this.$refs.input) {
+        this.$refs.input.focus()
+      }
     })
   },
 }
