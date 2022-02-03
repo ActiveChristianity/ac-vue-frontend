0b2b6a7464b7b6091e2d42f6485ccfe14f192ed0
Gerard <gerardlamo@gmail.com>
2022-02-03 19:59:52 +0100

    Add playlist function to play multiple tracks after each other.

--- src/single/GlobalPlayer.vue
+++ src/single/GlobalPlayer.vue
@@ -2,15 +2,27 @@
   <div class="fixed z-20 mb-16 md:mb-0 bottom-0 inset-x-0">
     <div class="w-full flex flex-col py-4">
       <div class="content-md pb-4 md:pt-4 relative">
-        <div class="mx-auto text-center z-10 relative">
-          <p v-if="$store.playing"
+        <div v-if="$store.playing" class="mx-auto text-center z-10 relative">
+          <g-link :to="$store.playing.playlist || ''"
              class="rounded-xl whitespace-no-wrap overflow-x-hidden max-w-full w-64 md:w-auto inline-block pb-1 px-2 border-2 border-slate-dark bg-gray-300 bg-opacity-75 shadow-2xl"
-             style="backdrop-filter: blur(3px); text-overflow: ellipsis">{{ $store.playing.title }}</p>
+             style="backdrop-filter: blur(3px); text-overflow: ellipsis">{{ $store.playing.title }}</g-link>
         </div>
         <vue-plyr ref="plyr" class="rounded-2xl shadow-xl border-2 border-slate-dark bg-gray-300 bg-opacity-50 -mt-4"
                   style="backdrop-filter: blur(3px);">
           <audio src="/ping.mp3"></audio>
         </vue-plyr>
+        <div v-if="hasTracks" class="z-20 absolute top-0 left-0 ml-2 md:mt-4">
+          <button class="p-1 border-2 border-slate-dark rounded-full bg-gray-100 bg-opacity-75 shadow-2xl"
+                  @click="previous"
+                  style="backdrop-filter: blur(3px);">
+            <icon name="fad-chevron-left" fa/>
+          </button>
+          <button class="p-1 border-2 border-slate-dark rounded-full bg-gray-100 bg-opacity-75 shadow-2xl"
+                  @click="next"
+                  style="backdrop-filter: blur(3px);">
+            <icon name="fad-chevron-right" fa/>
+          </button>
+        </div>
         <button class="z-20 absolute top-0 right-0 md:mt-4 mr-2 p-1 border-2 border-slate-dark rounded-full bg-gray-100 bg-opacity-75 shadow-2xl"
                 @click="close"
                 style="backdrop-filter: blur(3px);">
@@ -28,7 +40,9 @@ import 'plyr/dist/plyr.css'
 export default {
   data () {
     return {
-      current: null
+      current: null,
+      tracks: [],
+      trackIndex: 0,
     }
   },
   components: {
@@ -52,8 +66,23 @@ export default {
       paused () {
         return this.playing && this.playing.paused
       },
+      hasTracks () {
+        return this.tracks.length > 1
+      }
   },
   methods: {
+    previous () {
+      if (this.hasTracks) {
+        this.trackIndex = this.trackIndex - 1 > 0 ? this.trackIndex - 1 : this.tracks.length - 1
+        this.$store.playlist = this.tracks[this.trackIndex]
+      }
+    },
+    next () {
+      if (this.hasTracks) {
+        this.trackIndex = this.trackIndex + 1 < this.tracks.length ? this.trackIndex + 1 : 0
+        this.$store.playlist = this.tracks[this.trackIndex]
+      }
+    },
     close () {
       if (this.player) {
         this.player.stop()
@@ -71,9 +100,11 @@ export default {
         this.player.on('play', () => {
           this.$set(this.$store.playing, 'paused', false)
         })
+        this.player.on('ended', () => {
+          this.next()
+        })
       }
     })
-
   },
   watch: {
     playlist: {
@@ -81,6 +112,17 @@ export default {
       deep: true,
       handler (pl) {
         this.$nextTick(() => {
+          console.log(pl)
+          if (pl.tracks) {
+            if (this.tracks !== pl.tracks) {
+              this.tracks = pl.tracks
+            }
+
+            this.trackIndex = this.tracks.findIndex(t => t.url === pl.url)
+          } else {
+            this.tracks = []
+            this.trackIndex = 0
+          }
           if (! this.current || this.current.url !== pl.url) {
             this.playing = pl
             let source = {
--- src/templates/Playlist.vue
+++ src/templates/Playlist.vue
@@ -2,7 +2,7 @@
   <main class="content">
     <div class="fade-in pt-12">
       <div class="pb-8 text-center content-md">
-        <g-image v-if="playlist.image" class="w-full max-w-screen-md mx-auto md:rounded-2xl my-8" :src="playlist.image"></g-image>
+        <g-image v-if="playlist.image" class="w-full max-w-screen-md mx-auto rounded-2xl my-8" :src="playlist.image"></g-image>
         <h1 class="text-3xl text-blue-900 md:text-4xl font-medium leading-tight">{{ playlist.title }}</h1>
         <p>{{ playlist.excerpt }}</p>
       </div>
@@ -35,14 +35,14 @@ query Playlist ($id: ID!) {
         title
         duration
         url
-        image {
-          ...fImage
-        }
         post {
           id
           type,
           slug,
           readtime,
+          image {
+            ...fImage
+          }
           meta {
             as_ac
           }
@@ -65,16 +65,26 @@ export default {
       return this.$page.ql.playlist
     },
     tracks() {
-      return this.playlist.tracks.map(track => {
+      if (this.playlist._tracks) {
+        return this.playlist._tracks
+      }
+
+      const tracks = []
+      // Since most components are made to handle posts instead of tracks we just switch them.
+      this.playlist._tracks = this.playlist.tracks.map(track => {
         track.post.track = {
           title: track.title,
           url: track.url,
-          image: track.image,
-          duration: track.duration
+          duration: track.duration,
+          playlist: `/playlists/${this.playlist.slug}`,
+          tracks,
         }
         track.post.title = track.title
+        tracks.push(track.post.track)
         return track.post
       })
+
+      return this.playlist._tracks
     }
   }
 }
